b37335b07cfbfb6fce7aeb9bf811fd7f81db9ded
MazherRakam <megamiguel24@gmail.com>
2024-06-18 21:18:50 -0400

    Revert "seAgregoComentarios"
    This reverts commit 3935de2c31608645c01fe832626318bec49f6150.

--- Cache.h
+++ Cache.h
@@ -2,37 +2,37 @@
 #define Cache_h
 class Cache{
 	private:
-		unsigned long int etiqueta; //bits de Etiqueta
-		bool validez; //bits de validez
+		unsigned long int etiqueta;
+		bool validez;
 	public:
 /*********************************************************************************************************************************************************************/
 	//Constructores
-		Cache(){ //Inicializar cache
+		Cache(){
 			this->validez = false;
 			this->etiqueta = -1;
 		}
 		
-		Cache(unsigned long int etq,bool val){ //inicializar cache con su valores
+		Cache(unsigned long int etq,bool val){
 			this->validez = val;
 			this->etiqueta = etq;
 		}
 /*********************************************************************************************************************************************************************/
 		//getters
-		unsigned long int getEtiqueta(){ //Obtener Etiqueta
-			return (this->etiqueta); 
+		unsigned long int getEtiqueta(){
+			return (this->etiqueta);
 		}
 		
-		bool getValidez(){ //Obtener Validez
+		bool getValidez(){
 			return (this->validez);
 		}
 
 /*********************************************************************************************************************************************************************/
 		//Setters
-		void setEtiqueta(unsigned long int etq){ //insertar etiqueta
+		void setEtiqueta(unsigned long int etq){
 			this->etiqueta = etq;
 		}
 		
-		void setValidez(bool val){ //Insertar bits de validez
+		void setValidez(bool val){
 			this->validez = val;
 		}
 /*********************************************************************************************************************************************************************/
